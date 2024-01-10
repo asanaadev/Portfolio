36929bfc9a94bd0e603bafd2f72d6ed295c27f9d
Zhumabai00 <asanaalyev1@gmail.com>
2024-01-10 20:56:26 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Hero, Stars, Tech, Experience, About, Works, Feedbacks, Contact, Navbar
 function App() {
 
   return (
-    <div className="relative z-0 bg-primary">
+    <div className="relative z-0 bg-primary scroll-smooth">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
