d682592089e1f70e7fb95a9c9c87ea2381053953
asanaadev <asanaalyev1@gmail.com>
2024-03-21 13:10:24 +0700

    fixmobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { Hero, Stars, Tech, Experience, About, Works, Feedbacks, Contact, Navbar } from './components'
+import TabComponent from './Navigation'
 
 function App() {
 
   return (
     <div className="relative z-0 bg-primary scroll-smooth">
+      <TabComponent/>
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
